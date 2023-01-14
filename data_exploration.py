7c796133bb7e0e7cdc376070ad5bc8857cb6f23e
liljafreja <11898215+liljafreja@users.noreply.github.com>
2023-01-14 19:38:15 +0100

    simplify code to get the max wave height

--- data_exploration.py
+++ data_exploration.py
@@ -7,16 +7,15 @@ def load_df(file_name: str) -> pd.DataFrame:
         return wave_ds.to_dataframe()
 
 
-def find_maximal_wave_height(longitude: float, latitude: float, df: pd.DataFrame) -> float:
-    df = df[["longitude", "latitude", "hmax"]] \
-        .groupby(['longitude', 'latitude']) \
-        .aggregate('max') \
-        .reset_index()
-    return df[(df['longitude'] == longitude) & (df['latitude'] == latitude)]['hmax'].values[0]
+def find_maximal_wave_height(longitude: float, latitude: float, data: xr.Dataset) -> float:
+    df = data \
+        .resample(time='1D') \
+        .max() \
+        .sel({'longitude': longitude, 'latitude': latitude}, method='nearest') \
+        .to_pandas()
+    return df['hmax'].values[0]
 
 
 if __name__ == '__main__':
-    df = load_df("data/waves_2019-01-01.nc").reset_index()
-    df.info()
-
-    print(find_maximal_wave_height(0, 0, df))
+    with xr.open_dataset("data/waves_2019-01-01.nc") as ds:
+        print("The max wave height for the given coordinates is", find_maximal_wave_height(0, 0, ds), ".")
