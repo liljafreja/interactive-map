0d005b5df89c0c4453d358b71aa2d9a477b3d78d
liljafreja <11898215+liljafreja@users.noreply.github.com>
2023-01-15 13:54:42 +0100

    write tests for flask application both for returning wave values on land and on water

---
+++ backend/tests/test_wave_height_analysis.py
@@ -0,0 +1,16 @@
+from backend.wave_height_analysis import app
+import json
+
+
+def test_get_max_wave_height_on_water():
+    response = app.test_client().get('/max_wave_height',
+                                     query_string={'longitude': 0.0, 'latitude': 0.0})
+    assert response.status_code == 200
+    assert json.loads(response.data.decode('utf-8'))['maxWaveHeight'] == '2.3259907'
+
+
+def test_get_max_wave_height_on_land():
+    response = app.test_client().get('/max_wave_height',
+                                     query_string={'longitude': -0.09, 'latitude': 51.5})
+    assert response.status_code == 200
+    assert json.loads(response.data.decode('utf-8'))['maxWaveHeight'] == 'nan'
