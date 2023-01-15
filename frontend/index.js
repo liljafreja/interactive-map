6de7fb032806a02738f6e538a7b8cb07c7e18192
liljafreja <11898215+liljafreja@users.noreply.github.com>
2023-01-15 15:09:13 +0100

    set default coordinates to Oslo

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([59.8, 10.5], 9);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
